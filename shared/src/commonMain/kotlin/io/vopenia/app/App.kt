d8519106200f842ae774655107f538bb7f834b1b
Kévin Le Perf <codlabtech@gmail.com>
2024-07-01 03:45:18 +0200

    iOS startup

--- shared/src/commonMain/kotlin/io/vopenia/app/App.kt
+++ shared/src/commonMain/kotlin/io/vopenia/app/App.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.Composable
 @Composable
 fun App(
     isDarkTheme: Boolean,
-    onBackPressed: AppBackPressProvider
+    onBackPressed: AppBackPressProvider? = null
 ) {
 
 }
--- shared/src/commonMain/kotlin/io/vopenia/app/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.vopenia.app
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
