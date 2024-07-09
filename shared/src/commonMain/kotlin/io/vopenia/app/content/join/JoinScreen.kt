c046ac1e3b3009ad9f6cd62e83e5e1b7da3149f5
Kévin Le Perf <codlabtech@gmail.com>
2024-07-09 08:40:40 +0200

    Update room's form

--- shared/src/commonMain/kotlin/io/vopenia/app/content/join/JoinScreen.kt
+++ shared/src/commonMain/kotlin/io/vopenia/app/content/join/JoinScreen.kt
@@ -1,11 +1,15 @@
 package io.vopenia.app.content.join
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
@@ -17,11 +21,19 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.vopenia.app.res.Res
+import com.vopenia.app.res.join_background
+import eu.codlab.compose.theme.LocalDarkTheme
 import eu.codlab.compose.widgets.TextNormal
+import eu.codlab.safearea.views.SafeArea
 import eu.codlab.viewmodel.rememberViewModel
 import io.vopenia.app.LocalApp
+import io.vopenia.app.LocalFontSizes
 import io.vopenia.app.preview.PreviewWrapperLightColumn
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun JoinScreen(
@@ -31,50 +43,95 @@ fun JoinScreen(
     val model = rememberViewModel("join_screen") { JoinScreenModel(app) }
     val state by model.states.collectAsState()
 
-    Column(
+    Box(
         modifier = modifier.imePadding()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
     ) {
-        Box(
-            modifier = Modifier.widthIn(0.dp, 250.dp),
-            contentAlignment = Alignment.Center
+        if (LocalDarkTheme.current) {
+            Image(
+                painter = painterResource(Res.drawable.join_background),
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Column(
+            modifier = modifier.imePadding()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            val internalModifier = Modifier.fillMaxWidth()
-            Column {
-                OutlinedTextField(
-                    modifier = internalModifier,
-                    label = {
-                        TextNormal(
-                            text = "Participant's Name"
-                        )
-                    },
-                    value = state.participant,
-                    onValueChange = { model.participant = it }
-                )
 
-                OutlinedTextField(
-                    modifier = internalModifier,
-                    label = {
-                        TextNormal(
-                            text = "Room's Name"
-                        )
-                    },
-                    value = state.room,
-                    onValueChange = { model.room = it }
-                )
-
-                Button(
-                    modifier = internalModifier,
-                    enabled = model.room.text.isNotEmpty()
-                            && model.participant.text.isNotEmpty(),
-                    onClick = { model.join() }
-                ) {
+            Box(
+                modifier = Modifier.widthIn(0.dp, 250.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                val internalModifier = Modifier.fillMaxWidth()
+                Column {
                     TextNormal(
-                        text = "Join"
+                        fontSize = LocalFontSizes.current.joinRoom.subTitle,
+                        text = "Global"
                     )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    TextNormal(
+                        fontSize = LocalFontSizes.current.joinRoom.subTitle,
+                        text = "Real Time"
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    TextNormal(
+                        fontSize = LocalFontSizes.current.joinRoom.title,
+                        fontWeight = FontWeight.Bold,
+                        text = "Connectivity"
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        modifier = internalModifier,
+                        label = {
+                            TextNormal(
+                                text = "Participant's Name"
+                            )
+                        },
+                        value = state.participant,
+                        onValueChange = { model.participant = it }
+                    )
+
+                    OutlinedTextField(
+                        modifier = internalModifier,
+                        label = {
+                            TextNormal(
+                                text = "Room's Name"
+                            )
+                        },
+                        value = state.room,
+                        onValueChange = { model.room = it }
+                    )
+
+                    Button(
+                        modifier = internalModifier,
+                        enabled = model.room.text.isNotEmpty()
+                                && model.participant.text.isNotEmpty(),
+                        onClick = { model.join() }
+                    ) {
+                        TextNormal(
+                            text = "Join"
+                        )
+                    }
                 }
+            }
+        }
+
+        SafeArea {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextNormal(
+                    fontSize = LocalFontSizes.current.joinRoom.copyright,
+                    text = "Copyright © 2024 - The Renegades"
+                )
             }
         }
     }
