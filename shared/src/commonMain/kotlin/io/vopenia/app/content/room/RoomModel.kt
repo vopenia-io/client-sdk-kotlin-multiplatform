af01324978459ef0f65c1c36cad367c60360cfbf
Kévin Le Perf <codlabtech@gmail.com>
2024-07-11 05:36:28 +0200

    On model recreation, prevent crash

--- shared/src/commonMain/kotlin/io/vopenia/app/content/room/RoomModel.kt
+++ shared/src/commonMain/kotlin/io/vopenia/app/content/room/RoomModel.kt
@@ -1,6 +1,7 @@
 package io.vopenia.app.content.room
 
 import com.vopenia.sdk.Room
+import com.vopenia.sdk.compose.log
 import com.vopenia.sdk.participant.remote.RemoteParticipant
 import com.vopenia.sdk.participant.track.RemoteVideoTrack
 import com.vopenia.sdk.utils.Queue
@@ -25,7 +26,12 @@ class RoomModel(
     val queue = Queue()
 
     init {
-        launch {
+        launch(
+            onError = {
+                log("RoomModel", "Error ${it.message}")
+                it.printStackTrace()
+            }
+        ) {
             room.remoteParticipants.collect { participants ->
                 participants.forEach { participant ->
                     println("remoteParticipants -> looping through")
@@ -38,7 +44,12 @@ class RoomModel(
         }
     }
 
-    private suspend fun launchForParticipant(participant: RemoteParticipant) = launch {
+    private suspend fun launchForParticipant(participant: RemoteParticipant) = launch(
+        onError = {
+            log("RoomModel", "Error launchForParticipant ${it.message}")
+            it.printStackTrace()
+        }
+    ) {
         val originalTracks = states.value.participantCells
 
         if (null == originalTracks.find { it.participant == participant }) {
@@ -64,7 +75,12 @@ class RoomModel(
         participant: RemoteParticipant,
         videoTrack: RemoteVideoTrack
     ) {
-        launch {
+        launch(
+            onError = {
+                log("RoomModel", "Error launchForParticipantTrack ${it.message}")
+                it.printStackTrace()
+            }
+        ) {
             videoTrack.state.collect { newState ->
                 var originalTracks = states.value.participantCells
                 println("launchForParticipant -> on video track ${videoTrack.sid} -> ${newState}")
